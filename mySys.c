0d8fd1b962d89cbd0750bebe53de5b49e528bed8
evin0 <edodisho@ucsc.edu>
2022-06-22 01:11:30 -0700

    pushing all

---
+++ mySys.c
@@ -0,0 +1,14 @@
+// Program to essentialy recreate system
+// function from C standard library
+
+#include "mySys.h"
+#include <stdio.h>
+
+mySys(char *str) {
+    
+}
+
+int main() {
+    printf("Hello world\n");
+    return 0;
+}
---
+++ mySys.h
@@ -0,0 +1,1 @@
+char *mySys(char*);
